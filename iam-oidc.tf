5b26e586aa14e2f3ab71086690c53b0fcc42d199
kartik695 <arorakartik669@gmail.com>
2024-09-09 16:36:22 +0530

    2nd-day-commit

--- iam-oidc.tf
+++ iam-oidc.tf
@@ -1,4 +1,5 @@
 // oidc -> open id connect used for providing permission to pods 
+// To manage permissions for your applications that you deploy in Kubernetes. You can either attach policies to Kubernetes nodes directly. In that case, every pod will get the same access to AWS resources. Or you can create OpenID connect provider, which will allow granting IAM permissions based on the service account used by the pod.
 
 data "tls_certificate" "eks" {
     url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
--- nodes.tf
+++ nodes.tf
@@ -90,7 +90,6 @@ resource "aws_eks_node_group" "private-nodes" {
 
 
 
-
   ]
 }
 
